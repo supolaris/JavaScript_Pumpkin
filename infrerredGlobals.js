4272e20e950442f2beccb69fd70648f50b35e3bd
Muhammad Suleman <msulaman061@gmail.com>
2023-11-04 20:13:15 +0500

    infrerredGlobals

---
+++ infrerredGlobals.js
@@ -0,0 +1,21 @@
+var myHouse = {
+    book: "cats got wings"
+}
+
+
+function myBag() {
+    let a = 50;
+
+    function myNestedBag() {
+
+        myNewVar = "tested var";
+        return console.log(myHouse);
+
+        
+    }
+
+    myNestedBag();
+}
+
+myBag();
+
