a96032c8b2f7cdf37c7341b88fdc726a26180d46
Muhammad Suleman <msulaman061@gmail.com>
2023-11-05 10:02:31 +0500

    update

--- constructors.js
+++ constructors.js
@@ -1,13 +1,12 @@
- function myCar( name, engine, speed, windowTint, metallicColor , exhaust ){
+ function myCar( name, engine, speed, windowTint, metallicColor , exhaust, myDrive = function(){ return "My Fuled Cars"} ){
     this.myCarName = name;
     this.myCarEngine = engine;
     this.myCarSpeed = speed;
     this.myCarWiindowTint = windowTint;
     this.myCarMetallicColor = metallicColor;
     this.myCarExhaust = exhaust;
+    this.myCarDrive = myDrive;
  }
 
 
- var myCarOneDetails = new myCar( "audi", "9000hs", "350km", "shaded", true, 3 );
- var myCarTwoDetails = new myCar( "bmw", "6000hs", "250km", "faded", true, 2 );
- var myCarThreeDetails = new myCar( "audi", "3000hs", "150km", "melted", true, 1 );
+ var myCarOneDetails = new myCar( "audi", "9000hs", "350km", "shaded", true, 3, );
