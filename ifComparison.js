ffdeef47083f6c9b929971c53c23cf441a351571
Muhammad Suleman <msulaman061@gmail.com>
2023-11-06 09:46:35 +0500

    update

---
+++ ifComparison.js
@@ -0,0 +1,15 @@
+var aa = 10;
+
+if (aa ==  1){
+    console.log("Hello world");
+}else {
+    console.log("noot equal");
+}
+
+var myWord = "hello world"
+
+if (myWord ===  "hello world"){
+    console.log("Hello world");
+}else {
+    console.log("noot equal");
+}
