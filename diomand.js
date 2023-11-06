426c3c70fc66ae26597c83822a8e0438142e02ae
Muhammad Suleman <msulaman061@gmail.com>
2023-11-06 10:03:00 +0500

    update

--- diomand.js
+++ diomand.js
@@ -2,9 +2,16 @@ for(var i = 1; i<=10; i++){
 
     for(var j = 1; j<=i; j++){
 
+        if(i<(10/2)){
             document.write(" * ");
-        
+        }
     }
-    document.write("<br>");
-    
+
+    for(var k=10; k>i; k--){
+
+        if(i>(10/2)){
+            document.write(" * ");
+        }
+    }
+    document.write("<br>"); 
 }
